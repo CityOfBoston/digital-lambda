cb11f24ab13394ee14e87092a6493ffe4d7d97b6
Fin Hopkins <fin.hopkins@boston.gov>
2017-10-13 13:15:19 -0400

    Get clusterName in another place

--- python/instance-drain.py
+++ python/instance-drain.py
@@ -44,18 +44,14 @@ def checkContainerInstanceTaskStatus(Ec2InstanceId):
     clusterName = None
     tmpMsgAppend = None
 
-    # Describe instance attributes and get the Clustername from userdata section which would have set ECS_CLUSTER name
-    ec2Resp = ec2Client.describe_instance_attribute(InstanceId=Ec2InstanceId, Attribute='userData')
-    userdataEncoded = ec2Resp['UserData']
-    userdataDecoded = base64.b64decode(userdataEncoded['Value'])
-    logger.debug("Describe instance attributes response %s", ec2Resp)
+    ec2Resp = ec2Client.describe_instances(InstanceIds=[Ec2InstanceId])
+    logger.debug("Describe instances response %s",ec2Resp)
 
-    tmpList = userdataDecoded.split()
-    for token in tmpList:
-        if token.find("ECS_CLUSTER") > -1:
-            # Split and get the cluster name
-            clusterName = token.split('=')[1]
-            logger.info("Cluster name %s",clusterName)
+    tagList = ec2Resp['Reservations'][0]['Instances'][0]['Tags']
+    for tag in tagList:
+        if tag['Key'] == 'ClusterName':
+            clusterName = tag['Value']
+            logger.debug("Cluster name %s",clusterName)
 
     # Get list of container instance IDs from the clusterName
     paginator = ecsClient.get_paginator('list_container_instances')
@@ -130,11 +126,6 @@ def lambda_handler(event, context):
     logger.debug("Describe instances response %s",ec2Resp)
 
     tagList = ec2Resp['Reservations'][0]['Instances'][0]['Tags']
-
-    # userdataEncoded = ec2Resp['UserData']
-    # userdataDecoded = base64.b64decode(userdataEncoded['Value'])
-
-    # tmpList = userdataDecoded.split()
     for tag in tagList:
         if tag['Key'] == 'ClusterName':
             clusterName = tag['Value']
